27cdf6bd31ace52b1a30b732072fb9fce8564b98
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-12 04:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	const methods = Object.getOwnPropertyNames(Math);
+	const functionMethods = methods.filter(method => typeof method === 'function')
+	return functionMethods.join(', ');
 }
 
-alert(allMethods());
+// alert(allMethods());
